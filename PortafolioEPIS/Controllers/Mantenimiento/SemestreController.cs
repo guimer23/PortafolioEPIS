8648cdd877c4a5bc8850a9a53d66072eb242dfc7
NILTON EDY PEREZ MAMANI <nilperezm@upt.pe>
2019-05-16 13:39:11 -0500

    Semestre v1.0

--- PortafolioEPIS/Controllers/Mantenimiento/SemestreController.cs
+++ PortafolioEPIS/Controllers/Mantenimiento/SemestreController.cs
@@ -3,20 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PortafolioEPIS.Models;
 
 namespace PortafolioEPIS.Controllers.Mantenimiento
 {
     public class SemestreController : Controller
     {
-        // Accion Listar
+        //Listar
+        //Instanciar la clase Semestre
+        private Tbl_Semestre objSemestre = new Tbl_Semestre();
+
         public ActionResult Index()
         {
-            return View();
+            return View(objSemestre.Listar());
         }
-        // Accion Agregar
-        public ActionResult Agregar()
+
+        //Acction Visualizar
+
+        public ActionResult Visualizar(int id)
         {
-            return View();
+            return View(objSemestre.Obtener(id));
+        }
+
+        // Accion Agregar
+        public ActionResult Agregar(int id = 0)
+        {
+            return View(id == 0 ? new Tbl_Semestre() //agrega un nuevo objeto
+                           : objSemestre.Obtener(id)
+               );
+        }
+
+
+        //Action Guardar
+        public ActionResult Guardar(Tbl_Semestre objSemestre)
+        {
+            if (ModelState.IsValid)
+            {
+                objSemestre.Guardar();
+                return Redirect("~/Semestre");
+            }
+            else
+            {
+                return View("~/Views/Semestre/Agregar");
+            }
+        }
+
+        //Action Eliminar
+
+        public ActionResult Eliminar(int id)
+        {
+            objSemestre.Codigo_Semestre = id;
+            objSemestre.Eliminar();
+            return Redirect("~/Semestre");
         }
     }
 }
