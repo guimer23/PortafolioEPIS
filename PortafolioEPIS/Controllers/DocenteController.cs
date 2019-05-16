a6bc7fedaa62c7dc8e0097ed435c8549574f299e
GUIMER SENON COAQUIRA COAQUIRA <guicoaquirac@upt.pe>
2019-05-16 09:02:21 -0500

    Proyecto PortafolioEPIS

---
+++ PortafolioEPIS/Controllers/DocenteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortafolioEPIS.Controllers
+{
+    public class DocenteController : Controller
+    {
+        // Accion Listar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Accion Agregar
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortafolioEPIS/Controllers/Mantenimiento/ProfesionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortafolioEPIS.Controllers.Mantenimiento
+{
+    public class ProfesionController : Controller
+    {
+        // Accion Listar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Accion Agregar
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+    }
+}
