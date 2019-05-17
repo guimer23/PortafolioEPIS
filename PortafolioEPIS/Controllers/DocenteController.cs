5073e5737f2f7f3c47f620712300e47b316432e2
NILTON EDY PEREZ MAMANI <nilperezm@upt.pe>
2019-05-16 21:35:13 -0500

    Docente v1.0

--- PortafolioEPIS/Controllers/DocenteController.cs
+++ PortafolioEPIS/Controllers/DocenteController.cs
@@ -3,20 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PortafolioEPIS.Models;
 namespace PortafolioEPIS.Controllers
 {
     public class DocenteController : Controller
     {
-        // Accion Listar
+        //Instanciar la clase Semestre
+        private Tbl_Docente objDocente = new Tbl_Docente();
+
+        // Acction Listar
         public ActionResult Index()
         {
-            return View();
+            return View(objDocente.Listar());
         }
-        // Accion Agregar
-        public ActionResult Agregar()
+
+        //Acction Visualizar
+        public ActionResult Visualizar(int id)
         {
-            return View();
+            return View(objDocente.Obtener(id));
+        }
+
+        // Accion Agregar
+        public ActionResult Agregar(int id = 0)
+        {
+            return View(id == 0 ? new Tbl_Docente() //Agregar un nuevo objeto
+               : objDocente.Obtener(id));
+        }
+
+        //Action Guardar
+        public ActionResult Guardar(Tbl_Docente objDocente)
+        {
+            if (ModelState.IsValid)
+            {
+                objDocente.Guardar();
+                return Redirect("~/Docente");
+            }
+            else
+            {
+                return View("~/Views/Docente/Agregar");
+            }
+
+        }
+
+        //Action Eliminar
+
+        public ActionResult Eliminar(int id)
+        {
+            objDocente.Codigo_Docente = id;
+            objDocente.Eliminar();
+            return Redirect("~/Docente");
         }
     }
 }
--- PortafolioEPIS/Models/Tbl_Docente.cs
+++ PortafolioEPIS/Models/Tbl_Docente.cs
@@ -5,6 +5,8 @@ namespace PortafolioEPIS.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Linq;
+    using System.Data.Entity;
 
     public partial class Tbl_Docente
     {
@@ -76,5 +78,86 @@ namespace PortafolioEPIS.Models
         public virtual ICollection<Tbl_DetalleCargaAcademica> Tbl_DetalleCargaAcademica { get; set; }
 
         public virtual Tbl_Profesion Tbl_Profesion { get; set; }
+
+        //Metodo Listar
+        public List<Tbl_Docente> Listar()
+        {
+            var objDocente = new List<Tbl_Docente>();
+            try
+            {
+                using (var db = new Modelo_Portafolio())
+                {
+                    objDocente = db.Tbl_Docente.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objDocente;
+        }
+
+        //Metodo Obtener
+        public Tbl_Docente Obtener(int id)//retorna solo un objeto
+        {
+            var objDocente = new Tbl_Docente();
+            try
+            {
+                using (var db = new Modelo_Portafolio())
+                {
+                    objDocente = db.Tbl_Docente
+                                    .Where(x => x.Codigo_Docente == id)
+                                    .SingleOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objDocente;
+        }
+
+        //Metodo Guardar
+
+        public void Guardar()
+        {
+            try
+            {
+                using (var db = new Modelo_Portafolio())
+                {
+                    if (this.Codigo_Docente > 0)//sis existe un valor mayor a cero es porque existe registro
+                    {
+                        db.Entry(this).State = System.Data.Entity.EntityState.Modified;
+                    }
+                    else
+                    {
+                        //SINO EXISTE EL REGISTRO LO GRABA(nuevo)
+                        db.Entry(this).State = System.Data.Entity.EntityState.Added;
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        //Metodo Eliminar 
+        public void Eliminar()
+        {
+            try
+            {
+                using (var db = new Modelo_Portafolio())
+                {
+                    db.Entry(this).State = System.Data.Entity.EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
     }
 }
