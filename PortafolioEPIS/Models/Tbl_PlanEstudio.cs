7c3c628d272bc780dfddfa7527bf5404530b3708
JhonPeterAguilarAtencio <xxjhonxxaguilarxx@gmail.com>
2019-05-24 11:27:06 -0500

    Eliminar subi por las puras xd

--- PortafolioEPIS/Models/Tbl_PlanEstudio.cs
+++ PortafolioEPIS/Models/Tbl_PlanEstudio.cs
@@ -5,6 +5,7 @@ namespace PortafolioEPIS.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+
     using System.Linq;
     using System.Data.Entity;
 
