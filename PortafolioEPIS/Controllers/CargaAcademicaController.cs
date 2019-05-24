cb61d9902f34418a8974fd12591306a683342261
GUIMER SENON COAQUIRA COAQUIRA <guicoaquirac@upt.pe>
2019-05-24 15:30:37 -0500

    Actualizacion de Carga Academica

---
+++ PortafolioEPIS/Controllers/CargaAcademicaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortafolioEPIS.Controllers
+{
+    public class CargaAcademicaController : Controller
+    {
+        // Accion Listar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Accion Agregar
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+        // Accion Ver
+        public ActionResult Ver()
+        {
+            return View();
+        }
+    }
+}
