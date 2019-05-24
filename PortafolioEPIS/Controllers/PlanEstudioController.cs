7bf392b9202f2eab9fbb0eead45e9ffe11b0f808
NILTON EDY PEREZ MAMANI <nilperezm@upt.pe>
2019-05-24 10:46:19 -0500

    Crud de la tabla plan estudio

--- PortafolioEPIS/Controllers/PlanEstudioController.cs
+++ PortafolioEPIS/Controllers/PlanEstudioController.cs
@@ -3,25 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PortafolioEPIS.Models;
 
 namespace PortafolioEPIS.Controllers
 {
     public class PlanEstudioController : Controller
     {
-        // Accion Listar
+        private Tbl_PlanEstudio objPlanEstudio = new Tbl_PlanEstudio();
+        private Tbl_Semestre objSemestre = new Tbl_Semestre();
+
         public ActionResult Index()
         {
-            return View();
-        }        
-        // Accion Agregar
-        public ActionResult Agregar()
-        {
-            return View();
+            return View(objPlanEstudio.Listar());
         }
-        // Accion Ver
-        public ActionResult Ver()
+
+        //Accion Visualizar
+
+        public ActionResult Ver(int id)
         {
-            return View();
+            return View(objPlanEstudio.Obtener(id));
+        }
+
+        // Accion Agregar
+        public ActionResult Agregar(int id = 0)     
+        {
+            ViewBag.Tbl_Semestre = objSemestre.Listar();
+
+            return View(id == 0 ? new Tbl_PlanEstudio()//Agregar un nuevo objeto
+               : objPlanEstudio.Obtener(id));
+        }
+
+        //Action Guardar
+        public ActionResult Guardar(Tbl_PlanEstudio objPlanEstudio)
+        {
+            if (ModelState.IsValid)
+            {
+                objPlanEstudio.Guardar();
+                return Redirect("~/PlanEstudio");
+            }
+            else
+            {
+                return View("~/Views/PlanEstudio/Agregar.cshtml");
+            }
+
+        }
+
+        //Action Eliminar
+
+        public ActionResult Eliminar(int id)
+        {
+            objPlanEstudio.Codigo_PlanEstudio = id;
+            objPlanEstudio.Eliminar();
+            return Redirect("~/PlanEstudio");
         }
     }
 }
