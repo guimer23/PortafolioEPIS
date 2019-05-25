797b5e07e0c8337ee0be1a48c5d1e91ab0812f69
JhonPeterAguilarAtencio <xxjhonxxaguilarxx@gmail.com>
2019-05-25 04:13:27 -0500

    Agregado detalleCarga academica solo listar

--- PortafolioEPIS/Controllers/DetalleCargaAcademicaController.cs
+++ PortafolioEPIS/Controllers/DetalleCargaAcademicaController.cs
@@ -3,25 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PortafolioEPIS.Models;
 
 namespace PortafolioEPIS.Controllers
 {
     public class DetalleCargaAcademicaController : Controller
     {
-        // Accion Listar
+
+        private Tbl_DetalleCargaAcademica objDetalleCargaAcademica = new Tbl_DetalleCargaAcademica();
+        private Tbl_CargaAcademica objCargaAcademica = new Tbl_CargaAcademica();
+        private Tbl_PlanEstudio objPlanEstudio = new Tbl_PlanEstudio();
+        private Tbl_Docente objDocente = new Tbl_Docente();
+        private Tbl_Seccion objSeccion = new Tbl_Seccion();
+        private Tbl_DetallePlanEstudio objDetallePlanEstudio = new Tbl_DetallePlanEstudio();
+        private  Tbl_Semestre objSemestre= new Tbl_Semestre();
+
         public ActionResult Index()
         {
-            return View();
+            return View(objDetalleCargaAcademica.Listar());
         }
+
+        //Accion Visualizar
+
+        public ActionResult Ver(int id)
+        {
+            return View(objDetalleCargaAcademica.Obtener(id));
+        }
+
         // Accion Agregar
-        public ActionResult Agregar()
+        public ActionResult Agregar(int id = 0)
         {
-            return View();
+            
+            ViewBag.Tbl_CargaAcademica = objCargaAcademica.Listar();
+            ViewBag.Tbl_CargaAcademica = objPlanEstudio.Listar();
+            ViewBag.Tbl_CargaAcademica = objDocente.Listar();
+            ViewBag.Tbl_CargaAcademica = objSeccion.Listar();
+            ViewBag.Tbl_CargaAcademica = objDetallePlanEstudio.Listar();
+            ViewBag.Tbl_CargaAcademica = objSemestre.Listar();
+
+            return View(id == 0 ? new Tbl_DetalleCargaAcademica()//Agregar un nuevo objeto
+               : objDetalleCargaAcademica.Obtener(id));
         }
-        // Accion Ver
-        public ActionResult Ver()
+
+        //Action Guardar
+        public ActionResult Guardar(Tbl_DetalleCargaAcademica objDetalleCargaAcademica)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                objDetalleCargaAcademica.Guardar();
+                return Redirect("~/DetalleCargaAcademica");
+            }
+            else
+            {
+                return View("~/Views/DetalleCargaAcademica/Agregar.cshtml");
+            }
+
+        }
+
+        //Action Eliminar
+
+        public ActionResult Eliminar(int id)
+        {
+            objDetalleCargaAcademica.Codigo_DetalleCargaAcademica = id;
+            objDetalleCargaAcademica.Eliminar();
+            return Redirect("~/DetalleCargaAcademica");
         }
     }
 }
