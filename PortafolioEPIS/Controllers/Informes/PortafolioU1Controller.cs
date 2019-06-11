d777349ebea33536f362fe80c8d39c5d1040754b
J-Alfaro <J-Alfaro>
2019-06-11 18:19:28 +0100

    Guardado de aprobados , retirados y abandono

--- PortafolioEPIS/Controllers/Informes/PortafolioU1Controller.cs
+++ PortafolioEPIS/Controllers/Informes/PortafolioU1Controller.cs
@@ -25,5 +25,27 @@ namespace PortafolioEPIS.Controllers.Informes
             ViewBag.prueba = objPortafolio.Listar();
             return View(objDetalleCargaAcademica.Obtener(id));
         }
+
+        public ActionResult Guardar(Tbl_Portafolio objPortafolioU1, int idprueba,int retirados, int abandonos, int aprobados,int codigo,string estado,string unidad)
+        {
+
+            objPortafolioU1.Codigo_Portafolio = idprueba;
+            objPortafolioU1.Codigo_DetalleCargaAcademica = codigo;
+            objPortafolioU1.Retirados_Portafolio = retirados;
+            objPortafolioU1.Abandono_Portafolio = abandonos;
+            objPortafolioU1.Aprobados_Portafolio = aprobados;
+            objPortafolioU1.Unidad_Portafolio = unidad;
+            objPortafolioU1.Fecha_Portafolio = DateTime.Now;
+            objPortafolioU1.Estado_Portafolio = estado;
+            objPortafolioU1.Guardar();
+            return Redirect("~/PortafolioU1/Agregar/" + codigo);
+
+            //}
+            //else
+            //{
+            //    return View("~/Views/PruebaEntrada/Agregar.cshtml");
+            //}
+
+        }
     }
 }
