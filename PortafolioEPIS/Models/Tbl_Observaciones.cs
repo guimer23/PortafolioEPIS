f1e37a10f5a99556fe7d093cce47fe27b7051c29
Samuel <raseth2.cek22@gmail.com>
2019-06-19 15:00:37 -0500

    mio

--- PortafolioEPIS/Models/Tbl_Observaciones.cs
+++ PortafolioEPIS/Models/Tbl_Observaciones.cs
@@ -25,17 +25,17 @@ namespace PortafolioEPIS.Models
         [StringLength(250)]
         public string Silabo_Observaciones { get; set; }
 
-        public bool? MaterialCurso_Observaciones { get; set; }
+        public bool MaterialCurso_Observaciones { get; set; }
 
-        public bool? Cuestionarios_Observaciones { get; set; }
+        public bool Cuestionarios_Observaciones { get; set; }
 
-        public bool? TareasEncargadas_Observaciones { get; set; }
+        public bool TareasEncargadas_Observaciones { get; set; }
 
-        public bool? Foros_Observaciones { get; set; }
+        public bool Foros_Observaciones { get; set; }
 
-        public bool? ExamenesVirtuales_Observaciones { get; set; }
+        public bool ExamenesVirtuales_Observaciones { get; set; }
 
-        public bool? Slideshow_Observaciones { get; set; }
+        public bool Slideshow_Observaciones { get; set; }
 
         [StringLength(250)]
         public string Administrativas_Observaciones { get; set; }
@@ -53,5 +53,92 @@ namespace PortafolioEPIS.Models
         public string ComentariosRecomendaciones_Observaciones { get; set; }
 
         public virtual Tbl_InformeFinal Tbl_InformeFinal { get; set; }
+
+        //metodo listar
+        public List<Tbl_Observaciones> Listar()//Retorna una coleccion de registros
+        {
+            var objCargo = new List<Tbl_Observaciones>();
+            try
+            {
+                using (var db = new Modelo_Portafolio())
+                {
+                    objCargo = db.Tbl_Observaciones.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objCargo;
+        }
+
+        //metodo obtener
+        public Tbl_Observaciones Obtener(int id)//retorna solo un objeto
+        {
+            var objCargo = new Tbl_Observaciones();
+            try
+            {
+                using (var db = new Modelo_Portafolio())
+                {
+                    objCargo = db.Tbl_Observaciones
+                        .Where(x => x.Codigo_InformeFinal == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objCargo;
+        }
+
+        //metodo guardar
+        public void Guardar()//retorna solo un objeto
+        {
+
+            try
+            {
+                using (var db = new Modelo_Portafolio())
+                {
+                    if (this.Codigo_Observaciones > 0)
+                    {
+                        //si existe un valor mayor a 0 es porque existe un registro
+                        db.Entry(this).State = EntityState.Modified;
+
+                    }
+                    else
+                    {
+                        //si no existe registro graba(nuevo registro)
+                        db.Entry(this).State = EntityState.Added;
+
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        //metodo Eliminar
+        public void Eliminar()
+        {
+
+            try
+            {
+                using (var db = new Modelo_Portafolio())
+                {
+                    db.Entry(this).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
     }
 }
