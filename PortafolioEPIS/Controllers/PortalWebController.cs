0b45464978f6c5200795bfeb2750b9cf5c6894b9
GUIMER SENON COAQUIRA COAQUIRA <guicoaquirac@upt.pe>
2019-06-25 08:15:13 -0500

    Actualizacion de Interfaz PortalePIS

---
+++ PortafolioEPIS/Controllers/PortalWebController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortafolioEPIS.Controllers
+{
+    public class PortalWebController : Controller
+    {
+        // GET: PortalWeb
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
